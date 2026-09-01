3998e5aea693205449d8e8b700ce911edda33004
GauravWardhekara <gwardhekar@aroraengineers.com>
2026-09-01 17:13:32 +0530

    feat: implement PWA service worker support and update iOS Swift Package Manager dependencies

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.5.0"),
-        .package(name: "CapacitorCamera", path: "..\..\..\node_modules\@capacitor\camera")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.5.1"),
+        .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera")
     ],
     targets: [
         .target(
